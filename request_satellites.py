f47873bcf23507e4e6c43d831d1347562f25231d
JusticeBeaver5 <ruslanshaov@gmail.com>
2023-05-28 05:42:22 +0300

    some adjustments

--- request_satellites.py
+++ request_satellites.py
@@ -6,6 +6,8 @@ One request returns json with more than 20 000 satellites.
 Ange is resposoble for how many satellites will be requested. 1 returns smallest possible amount, 180 returns
 everything.
 
+for some reason dict saves less data???
+
 '''
 
 import requests
@@ -18,7 +20,7 @@ load_dotenv()
 
 key = os.environ.get('TOKEN')
 
-angle = 180
+angle = 360
 
 
 def get_sat_count(angle, key):
@@ -32,9 +34,15 @@ def make_request(angle, key):
     return r.json()
 
 
-def write_csv(data):
-    with open('database.csv','a', encoding='utf-8') as file:
-        writer = csv.writer(file, delimiter=',', lineterminator = '\n')
+def write_csv_dict(data):
+    with open('database_dict.csv','w', encoding='utf-8') as f:
+        for key in data.keys():
+            f.write("%s, %s\n" % (key, data[key]))
+
+
+def write_to_csv_list(data):
+    with open('database_list.csv','w', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
         writer.writerows(data)
 
 
@@ -42,10 +50,23 @@ data = make_request(angle, key)
 
 
 
-for i, item in enumerate(data['above']):
-    name = data['above'][i]['satname']
-    lat = data['above'][i]['satlat']
-    long = data['above'][i]['satlng']
-    alt = data['above'][i]['satalt'] 
-    satid = data['above'][i]['satid']
-    print(name)
+
+def filter_data(data):
+    satellites = {}
+    names = []
+    for i, _ in enumerate(data['above']):
+        name = data['above'][i]['satname']
+        satid = data['above'][i]['satid']
+        # lat = data['above'][i]['satlat']
+        # long = data['above'][i]['satlng']
+        # alt = data['above'][i]['satalt'] 
+        satellites = {**satellites, name:satid}
+        # names.append([name])
+    # return names
+    return satellites
+
+# print(filter_data(data))
+
+write_csv_dict(filter_data(data))
+
+# write_to_csv_list(filter_data(data))
